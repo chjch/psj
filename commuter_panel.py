7eaa0bc6718f3ef4cff6647d7781f26e1c1a938f
chjch <chj.chen@hotmail.com>
2022-10-12 14:44:08 -0400

    update slider

--- commuter_panel.py
+++ commuter_panel.py
@@ -7,12 +7,32 @@ njob_options = {
     'More than 30 jobs': (4, 5)
 }
 
+commuter_dropdown = dcc.Dropdown(id='njob-picker',
+                                 className='dropdown',
+                                 options=list(njob_options.keys()))
+
+commuter_range_slider = dcc.RangeSlider(
+    min=0,
+    max=300,
+    step=None,
+    marks={
+        0: '0',
+        5: '5',
+        10: '10',
+        15: '15',
+        20: '20',
+        25: '25',
+        30: '30',
+        300: 'over 30'
+    },
+    id='njob-slider'
+)
+
 commuter_panel = html.Div(
     className='pretty_container',
     children=[
         html.Div("Filter by Number of Jobs"),
-        dcc.Dropdown(id='njob-picker',
-                     className='dropdown',
-                     options=list(njob_options.keys()))
+        commuter_dropdown
+        # commuter_range_slider
     ]
 )
