025c71fd36f2bd06c455758030747dc87b067254
JSoaresPaulino <marcelo.paulino71122@gmail.com>
2024-01-10 18:21:51 -0500

    added disclaimer modal, improved design of home screen on mobile, added dropdown for categories on mobile

--- app/navbar.py
+++ app/navbar.py
@@ -1,4 +1,4 @@
-from dash import html
+from dash import html, dcc, callback, Input, Output, State, no_update
 import dash_bootstrap_components as dbc
 
 NAVBAR_LOGO = "/assets/image/UFlogo.png"
@@ -69,8 +69,41 @@ navbar_right = dbc.Row(
         className="g-0 flex-nowrap mt-3 mt-md-0",
         id="navbar-links-group"
     ),
-    className="ms-auto",
+    className="ms-auto hidden-1500",
     # align="center",
+)
+
+dropdown = html.Div(
+    [
+        dbc.DropdownMenu(
+            [
+                dbc.DropdownMenuItem(
+                    "HOUSING", id="dropdown-button", href="/viewer/housing",
+                ),
+                dbc.DropdownMenuItem(
+                    "TRANSPORTATION", href="/viewer/transportation",
+                ),
+                dbc.DropdownMenuItem(
+                    "CRITICAL INFRASTRUCTURE", href="/viewer/critical-infrastructure",
+                ),
+                dbc.DropdownMenuItem(
+                    "COMMUNITY SERVICES", href="/viewer/community-services",
+                ),
+                dbc.DropdownMenuItem(
+                    "NATURAL & CULTURAL", href="/viewer/natural-cultural-resources",
+                ),
+                dbc.DropdownMenuItem(
+                    "LOCAL ECONOMY", href="/viewer/local-economy",
+                ),
+                dbc.DropdownMenuItem(
+                    "ADAPTATION", href="/viewer/adaptation",
+                ),
+            ],
+            label="Category",
+            align_end=True,
+            className="show-1500"
+        ),
+    ]
 )
 
 navbar = dbc.Navbar(
@@ -78,12 +111,8 @@ navbar = dbc.Navbar(
         [
             navbar_brand,
             dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
-            dbc.Collapse(
-                navbar_right,
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True
-            )
+            navbar_right,
+            dropdown,
         ],
         fluid=True,
         class_name='px-4',
@@ -94,3 +123,11 @@ navbar = dbc.Navbar(
     class_name='px-4 py-3',
     # style={'box-shadow': '1px 1px 1px black'}
 )
+
+@callback(
+    Output("item-clicks", "children"), [Input("dropdown-button", "n_clicks")]
+)
+def count_clicks(n):
+    if n:
+        return f"Button clicked {n} times."
+    return "Button not clicked yet."
--- app/pages/home.py
+++ app/pages/home.py
@@ -1,8 +1,42 @@
 import dash
-from dash import html
+from dash import Input, Output, State, html, callback
 import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path='/', title='Port St. Joe Dashboard')
+
+@callback(
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"), Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+disclaimer = """
+    The data and maps in this tool illustrate the potential
+    scale and extent of flooding, not the exact location;
+    they do not account for erosion, subsidence, or future
+    construction.
+    Although every effort has been made to ensure that
+    information is comprehensive and accurate, errors and
+    omissions may exist.
+    The data and the information included therein is
+    provided on an "as is" basis.
+    The Florida Institute for Built Environment Resilience
+    (FIBER), Florida Sea Grant, the University of Florida,
+    or any of their respective faculty, staff, or
+    administration specifically disclaim any warranty,
+    either expressed or implied, including but not limited
+    to the implied warranties of merchantability and
+    fitness for a particular use.
+    The entire risk as to quality and performance is with
+    the user.
+    This tool should be used strictly as a planning
+    reference tool and not for navigation, permitting, or
+    other legal purposes.
+"""
 
 layout = html.Div(
     children=[
@@ -22,6 +56,7 @@ layout = html.Div(
                 Use this dashboard to explore the impacts of different storm
                 events and sea-level rise scenarios.
             ''',
+            className="hidden-mobile",
             style={
                 "color": "white",
                 "width": "30%",
@@ -66,6 +101,7 @@ layout = html.Div(
             children=[
                 html.Div(
                     children='Disclaimer',
+                    className="hidden-mobile",
                     style={
                         "color": "white",
                         "font-weight": "bold",
@@ -73,29 +109,8 @@ layout = html.Div(
                     }
                 ),
                 html.Div(
-                    children='''
-                        The data and maps in this tool illustrate the potential
-                        scale and extent of flooding, not the exact location;
-                        they do not account for erosion, subsidence, or future
-                        construction.
-                        Although every effort has been made to ensure that
-                        information is comprehensive and accurate, errors and
-                        omissions may exist.
-                        The data and the information included therein is
-                        provided on an "as is" basis.
-                        The Florida Institute for Built Environment Resilience
-                        (FIBER), Florida Sea Grant, the University of Florida,
-                        or any of their respective faculty, staff, or
-                        administration specifically disclaim any warranty,
-                        either expressed or implied, including but not limited
-                        to the implied warranties of merchantability and
-                        fitness for a particular use.
-                        The entire risk as to quality and performance is with
-                        the user.
-                        This tool should be used strictly as a planning
-                        reference tool and not for navigation, permitting, or
-                        other legal purposes.
-                    ''',
+                    children=disclaimer,
+                    className="hidden-mobile",
                     style={
                         "color": "#dddddd",
                         "font-size": "0.8em",
@@ -108,10 +123,30 @@ layout = html.Div(
                 "text-align": "center",
                 "margin-left": "auto",
                 "margin-right": "auto",
-                "bottom": "100px",
-                "position": "absolute",
+                "position": "relative",
+                "padding-top": "50px",
             }
-        )
+        ),
+        dbc.Button(html.I(className="fa-solid fa-info", style={"margin-left":"auto", "margin-right": "auto"}),className="show-mobile info-btn", id="open", n_clicks=0, 
+                   style={
+                       "height": "35px", 
+                       "width": "35px", 
+                       "margin-top": "25px"
+                       }
+                    ),
+        dbc.Modal(
+            [
+                dbc.ModalHeader(dbc.ModalTitle("Disclaimer"), close_button=True, className="disclaimer-header btn-close-white"),
+                dbc.ModalBody(disclaimer),
+                dbc.ModalFooter(
+                    dbc.Button(
+                        "Close", id="close", className="ms-auto show-mobile", n_clicks=0
+                    )
+                ),
+            ],
+            id="modal",
+            is_open=False,
+        ),
     ],
     style={
         "background-image": "url('../assets/image/splash_bg.png')",
